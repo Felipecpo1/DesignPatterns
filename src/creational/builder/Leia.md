5066f0f0e2fe92905624cef695a72aecfdb8d15f
Felipe <felipeppt1@gmail.com>
2024-06-20 21:40:27 -0300

    Builder

--- src/creational/builder/Leia.md
+++ src/creational/builder/Leia.md
@@ -1,2 +1,5 @@
 O professor criou classes abstratas para o meal box, e classes abstratas para os meals. Assim como suas classes concretas.
 Uma meal box é composta por meals. Assim, foi explicado o padrão Composed, usado no ensinamento para chegar ao Builder.
+Os builders podem criar objetos completamente diferentes.
+No projeto apresentado, o meal-builder-protocol é a interface responsavel por controlar essa criação.
+Depois foi criado uma classe para implementar a interface mencionada acima, chamado main-dish-builder.ts.
--- src/creational/builder/index.ts
+++ src/creational/builder/index.ts
@@ -1,11 +1,40 @@
+
+/*
+O builder evita que o codigo seja criado desse tamanho
+------------------------------------------------------
 import { MealBox } from "./classes/meal-box";
 import { Beans, Meat, Rice } from "./classes/meals";
 
 const rice = new Rice("Arroz", 5);
 const beans = new Beans("Feijao", 15);
-const meat = new Meat("Carne", 25);
+const meat = new Meat("Carne", 5);
 const mealBox = new MealBox();
 mealBox.add(rice, beans, meat);
 console.log(mealBox)
-console.log(mealBox.getPrice())
+console.log(mealBox.getPrice())]
+------------------------------------------------------
+e seja criado como abaixo:
+*/
+
+import { MainDishBuilder } from "./classes/make-dish-builder";
+import { VeganDishBuilder } from "./classes/vegan-dish-builder";
+
+const mainDishBuilder = new MainDishBuilder();
+mainDishBuilder.makeMeal().makeDessert();
+console.log(mainDishBuilder.getMeal())
+console.log(mainDishBuilder.getPrice())
+
+mainDishBuilder.reset()
+
+/// criando outro obj com o builder
+const meal2 = mainDishBuilder.makeBeverage().getMeal();
+console.log(meal2)
+
+/// isso tira a complexidade e o conhecimento das classes usadas no cliente, que é o index
+/// desacoplando o codigo
+
+const veganDishBuilder = new VeganDishBuilder();
+const veganMeal = veganDishBuilder.makeMeal().getMeal();
+console.log(veganMeal)
+console.log(veganMeal.getPrice())
 
